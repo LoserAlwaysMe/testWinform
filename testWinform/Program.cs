e08a787ecf395f6e5b9d5117c67f245b669d753a
VoHoangAn <vohoangan2000@gmail.com>
2020-04-05 10:18:18 +0700

    fix program.cs

--- testWinform/Program.cs
+++ testWinform/Program.cs
@@ -14,6 +14,7 @@ namespace testWinform
         [STAThread]
         static void Main()
         {
+            MessageBox.Show("master");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
